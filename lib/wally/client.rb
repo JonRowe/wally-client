9ec853683c60bb51706e6dcb6afe5f8a84385181
Jon Rowe <hello@jonrowe.co.uk>
2014-07-15 22:27:11 +1000

    finally extract option parsing logic

--- lib/wally/client.rb
+++ lib/wally/client.rb
@@ -2,6 +2,7 @@ require 'wally/client/version'
 
 module Wally
   module Client
+    autoload 'CLI',     'wally/client/cli'
     autoload 'Command', 'wally/client/command'
     autoload 'Parser',  'wally/client/parser'
   end
